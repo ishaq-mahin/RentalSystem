060fa36508b9786f3eaef0f542ac2833142d6220
ishaq-mahin <ishaqmahin255@gmail.com>
2026-04-20 00:08:52 +0600

    Add files via upload

--- db.php
+++ db.php
@@ -1,8 +1,15 @@
 <?php
-$host = "localhost";
-$user = "root"; 
-$pass = "";     
-$dbname = "rental_db";
+// Look in your InfinityFree Control Panel (under MySQL Databases) for the exact Hostname
+$host = "sqlxxx.infinityfree.com"; 
+
+// This is the username from your error message earlier!
+$user = "if0_41696255"; 
+
+// This is the hidden vPanel/Hosting password you used earlier
+$pass = "your_hidden_vpanel_password"; 
+
+// This is the exact database name you clicked on in phpMyAdmin
+$dbname = "if0_41696255_rental"; 
 
 $conn = mysqli_connect($host, $user, $pass, $dbname);
 
